dcb300120ef27fbf0e9fffede313cfa698e39a43
crazylawn <devv1231@gmail.com>
2022-05-02 09:48:29 +0900

    Fix : mainPage

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import tw from 'twin.macro';
+import MemoPad from '@components/memoPad';
 const Main = () => {
-    return (
-        <div>
-            메인화면입니다!
-        </div>
-    );
+  return (
+    <div>
+      <Text>오늘의 할일 (9가지마법)</Text>
+      <MemoPad />
+    </div>
+  );
 };
+
+const Text = tw.div`
+text-grayish-red
+`;
 
 export default Main;
