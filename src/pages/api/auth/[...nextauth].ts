2a813a6a5ae060f2315bcb4d5f81cd85751290cc
crazylawn <devv1231@gmail.com>
2022-05-05 18:59:20 +0900

    🏦feat : prisma migrations

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,8 @@ import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import KakaoProvider from 'next-auth/providers/kakao';
 import NaverProvider from 'next-auth/providers/naver';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 export default NextAuth({
   // Configure one or more authentication providers
 
@@ -15,4 +17,32 @@ export default NextAuth({
       clientSecret: process.env.NAVER_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    signIn: async ({ user, account, profile }: any) => {
+      console.log(user, account, profile, 'try signin');
+
+      try {
+        const exitedUser = await prisma.user.findUnique({
+          where: { email: user.email },
+        });
+        if (exitedUser) {
+          console.log(exitedUser, 'get user');
+          return Promise.resolve(true);
+        } else {
+          console.log('create###', user.name, user.email);
+          const createdUser = await prisma.user.create({
+            data: {
+              name: user.name,
+              email: user.email,
+            },
+          });
+          console.log(createdUser, 'created user!');
+          return Promise.resolve(true);
+        }
+      } catch (e) {
+        console.log(e);
+        return Promise.reject(true);
+      }
+    },
+  },
 });
