bf539b3fdb4dcc657e5d0a40a9e560cbeaff0ebb
crazylawn <devv1231@gmail.com>
2022-06-02 16:04:48 +0900

    Rename : Accordion =>TargetAccordion

--- src/components/Accordion/TargetAccordion.stories.tsx
+++ src/components/Accordion/TargetAccordion.stories.tsx
@@ -1,10 +1,10 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import Accordion from '.';
+import { TargetAccordion } from './TargetAccordion';
 
 export default {
   title: 'Example/Accordion', //스토리북 폴더 걔층구조
-  component: Accordion, //옵셔널이지만 addon을 위해 써주는게 권장
-} as ComponentMeta<typeof Accordion>;
+  component: TargetAccordion, //옵셔널이지만 addon을 위해 써주는게 권장
+} as ComponentMeta<typeof TargetAccordion>;
 
 //   const Template: ComponentStory<typeof Accordion> = (args) => (
 //     <Accordion {...args} /> //args  story의 props 이자 input
