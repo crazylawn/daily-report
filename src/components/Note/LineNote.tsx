9803e887dcd94351e7b73783506596fd0683c11e
crazylawn <devv1231@gmail.com>
2022-06-20 21:14:33 +0900

    ✨feat : LineNote

---
+++ src/components/Note/LineNote.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import tw, { styled } from 'twin.macro';
+const LineNote = () => {
+  return <LineNoteWrapper></LineNoteWrapper>;
+};
+
+const LineNoteWrapper = tw.div`
+w-full
+h-full
+bg-white
+`;
+export default LineNote;
